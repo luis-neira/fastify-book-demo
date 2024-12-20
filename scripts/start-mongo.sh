1c072f8e8b63ea44497eb2a2cee9043fc4d21b74
Luis Neira <luis.neira@lossexpress.com>
2024-12-20 03:39:28 -0500

    update start-mongo script

--- scripts/start-mongo.sh
+++ scripts/start-mongo.sh
@@ -5,6 +5,20 @@ if [ -f .env ]; then
     export $(cat .env | xargs)
 fi
 
+# Required environment variables
+required_vars=(
+    "MONGO_INITDB_ROOT_USERNAME"
+    "MONGO_INITDB_ROOT_PASSWORD"
+)
+
+# Check if all required environment variables are set
+for var in "${required_vars[@]}"; do
+    if [ -z "${!var}" ]; then
+        echo "Error: Environment variable $var is not set." >&2
+        exit 1
+    fi
+done
+
 # Run Docker with dynamic environment variables
 docker run -d \
     -p 27017:27017 \
