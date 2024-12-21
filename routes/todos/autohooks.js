ea8eba573b4476b919dbc42bf77b2d47ff7dcefb
Luis Neira <luis.neira@lossexpress.com>
2024-12-21 03:53:42 -0500

    update autohooks

--- routes/todos/autohooks.js
+++ routes/todos/autohooks.js
@@ -4,5 +4,24 @@ const fp = require('fastify-plugin')
 const schemas = require('./schemas/loader')
 
 module.exports = fp(async function todoAutoHooks (fastify, opts) {
+  const todos = fastify.mongo.db.collection('todos')
+
   fastify.register(schemas)
+
+  fastify.decorate('mongoDataSource', {
+    async createTdo ({ title }) {
+      const _id = new fastify.mongo.ObjectId()
+      const now = new Date()
+      const { insertedId } = await todos.insertOne({
+        _id,
+        title,
+        done: false,
+        id: _id,
+        createdAt: now,
+        modifiedAt: now
+      })
+
+      return insertedId
+    }
+  })
 })
