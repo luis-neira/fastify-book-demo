d81ba03a7e62c21aca41eaca48d4faf9f534c8b4
Luis Neira <luis.neira@lossexpress.com>
2024-12-21 23:30:14 -0500

    add auth plugin

---
+++ plugins/auth.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const fp = require('fastify-plugin')
+const fastifyJwt = require('@fastify/jwt')
+
+module.exports = fp(async function authenticationPlugin (fastify, opts) {
+  const revokedTokens = new Map()
+
+  fastify.register(fastifyJwt, {
+    secret: fastify.secret.JWT_SECRET,
+    trusted: function isTrusted (request, decoratedToken) {
+      return !revokedTokens.has(decoratedToken.jti)
+    }
+  })
+
+  fastify.decorate('authenticate', async function authenticate (request, reply) {
+    try {
+      await request.jwtVerify()
+    } catch (err) {
+      reply.send(err)
+    }
+  })
+
+  fastify.decorateRequest('revokeToken', function () {
+    revokedTokens.set(this.user.jti, true)
+  })
+
+  fastify.decorateRequest('generateToken', async function () {
+    const token = await fastify.jwt.sign({
+      id: String(this.user._id),
+      username: this.user.username
+    }, {
+      jti: String(Date.now()),
+      expiresIn: fastify.secrets.JWT_EXPIRE_IN
+    })
+
+    return token
+  })
+}, {
+  name: 'authentication-plugin',
+  dependencies: ['application-config']
+})
